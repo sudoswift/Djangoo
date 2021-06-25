a0ad9f869add68d2dce67d1cb1722d32a99533e2
sudoswift <sudoswift@gmail.com>
2021-06-25 23:33:01 +0900

    include구문을 통해 urls configuration

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')) # 프로젝트의 urls.py에서 include 구문을 이용하면 내가 만든 Django app의 urls.py와 연동해 url을 맵핑할 수 있다. 내 app에서 urls 주소는 .../polls/내app의 urls.py에서 지정한 이름
 ]
