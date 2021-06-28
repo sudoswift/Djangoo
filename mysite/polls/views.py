6225992e6d97adb5f2dd4531762b4db368ca07f3
sudoswift <sudoswift@gmail.com>
2021-06-28 14:30:29 +0900

    add django generic view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
- 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id) # 이 question이
-    return render(request, 'polls/detail.html', {'question': question}) # 이 question이다. dictionary -> key : value
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
